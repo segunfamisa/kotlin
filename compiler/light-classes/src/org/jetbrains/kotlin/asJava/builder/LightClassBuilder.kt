4061e0613ac6c1e2a54e3f2256a77c83d945377b
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2018-02-16 16:08:41 +0300

    LightClassBuilder: override getFileResolveScope() for fake common file
    Not quite sure does it really affect results in platform or not

--- compiler/light-classes/src/org/jetbrains/kotlin/asJava/builder/LightClassBuilder.kt
+++ compiler/light-classes/src/org/jetbrains/kotlin/asJava/builder/LightClassBuilder.kt
@@ -23,16 +23,21 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.SystemInfo
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.ClassFileViewProvider
+import com.intellij.psi.FileResolveScopeProvider
 import com.intellij.psi.PsiManager
 import com.intellij.psi.impl.compiled.ClsFileImpl
 import com.intellij.psi.impl.java.stubs.PsiJavaFileStub
 import com.intellij.psi.impl.java.stubs.impl.PsiJavaFileStubImpl
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.stubs.PsiClassHolderFileStub
 import org.jetbrains.kotlin.codegen.state.GenerationState
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.resolve.BindingContext
+import org.jetbrains.kotlin.resolve.MultiTargetPlatform
 import org.jetbrains.kotlin.resolve.diagnostics.Diagnostics
+import org.jetbrains.kotlin.resolve.getMultiTargetPlatform
 
 data class LightClassBuilderResult(val stub: PsiJavaFileStub, val bindingContext: BindingContext, val diagnostics: Diagnostics)
 
@@ -46,7 +51,7 @@ fun buildLightClass(
     val project = files.first().project
 
     try {
-        val classBuilderFactory = KotlinLightClassBuilderFactory(createJavaFileStub(project, packageFqName, files))
+        val classBuilderFactory = KotlinLightClassBuilderFactory(createJavaFileStub(project, packageFqName, files, context))
         val state = GenerationState.Builder(
                 project,
                 classBuilderFactory,
@@ -73,23 +78,49 @@ fun buildLightClass(
     }
 }
 
-private fun createJavaFileStub(project: Project, packageFqName: FqName, files: Collection<KtFile>): PsiJavaFileStub {
+private fun createJavaFileStub(
+    project: Project,
+    packageFqName: FqName,
+    files: Collection<KtFile>,
+    context: LightClassConstructionContext
+): PsiJavaFileStub {
     val javaFileStub = PsiJavaFileStubImpl(packageFqName.asString(), /*compiled = */true)
     javaFileStub.psiFactory = ClsWrapperStubPsiFactory.INSTANCE
 
     val manager = PsiManager.getInstance(project)
 
     val virtualFile = getRepresentativeVirtualFile(files)
-    val fakeFile = object : ClsFileImpl(ClassFileViewProvider(manager, virtualFile)) {
-        override fun getStub() = javaFileStub
-
-        override fun getPackageName() = packageFqName.asString()
-
-        override fun isPhysical() = false
+    val fakeFile = when (context.module.getMultiTargetPlatform()) {
+        MultiTargetPlatform.Common -> FakeCommonClsFileImpl(javaFileStub, packageFqName, manager, virtualFile)
+        else -> FakeClsFileImpl(javaFileStub, packageFqName, manager, virtualFile)
     }
 
     javaFileStub.psi = fakeFile
     return javaFileStub
+}
+
+private open class FakeClsFileImpl(
+    private val javaFileStub: PsiClassHolderFileStub<*>,
+    private val packageFqName: FqName,
+    manager: PsiManager,
+    virtualFile: VirtualFile
+) : ClsFileImpl(ClassFileViewProvider(manager, virtualFile)) {
+    override fun getStub() = javaFileStub
+
+    override fun getPackageName() = packageFqName.asString()
+
+    override fun isPhysical() = false
+}
+
+private class FakeCommonClsFileImpl(
+    javaFileStub: PsiClassHolderFileStub<*>,
+    packageFqName: FqName,
+    manager: PsiManager,
+    virtualFile: VirtualFile
+) : FakeClsFileImpl(javaFileStub, packageFqName, manager, virtualFile), FileResolveScopeProvider {
+    override fun getFileResolveScope(): GlobalSearchScope = GlobalSearchScope.allScope(manager.project)
+
+    override fun ignoreReferencedElementAccessibility() = false
 }
 
 private fun getRepresentativeVirtualFile(files: Collection<KtFile>): VirtualFile {
